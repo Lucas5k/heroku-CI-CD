c14008c7660682517e7eb194ad89722b02c2cd82
Lucas5k <lucascabral112347@gmail.com>
2022-07-22 15:07:56 -0300

    Meu primeiro commit na plataforma heroku com Docker

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv/config');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (_req, res) => res.send(`Olá Marilene, estamos na porta ${port}`));
+
+app.listen(port, () => console.log(`Online: ${port}`));
